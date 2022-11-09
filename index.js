2da5771b5757038b3854addc0d382be700f7be0a
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-11-09 19:58:39 +0600

    get all services api done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // imports
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
+// define port and app
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -13,6 +16,32 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server running successfully!!");
 });
+
+// mongodb server connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.afwac63.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// setting up the apis
+
+async function run() {
+  const serviceCollection = client.db("serviceReview").collection("services");
+
+  app.get("/services", async (req, res) => {
+    const query = {};
+    const cursor = serviceCollection.find(query);
+    const result = await cursor.toArray();
+    res.send({
+      status: "true",
+      data: result,
+    });
+  });
+}
+
+run().catch((error) => console.error(error));
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
